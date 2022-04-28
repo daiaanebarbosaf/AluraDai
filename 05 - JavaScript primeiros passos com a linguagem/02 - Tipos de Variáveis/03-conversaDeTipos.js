ac48c2b0b96a2d759d509b5b3cf393ad185e85fb
Daiane Barbosa Farias <daiaanebarbosaf@gmail.com>
2022-04-28 12:25:03 -0300

    Create 03-conversaDeTipos.js

---
+++ 05 - JavaScript primeiros passos com a linguagem/02 - Tipos de Variáveis/03-conversaDeTipos.js
@@ -0,0 +1,11 @@
+console.log("Conversão de Tipos");
+
+console.log("2" + "2");
+
+//como fazer conversões
+console.log(parseInt("2") + parseInt("2"));
+
+//caso de conversao implicita, pois mesmo sendo texto, a linguagem consegue entender e dividir o valor
+console.log("10" / "2");
+//Vai retornar NaN (Not a Number)
+console.log("Ricardo" / "2"); 
